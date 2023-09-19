e58f9a551e26568b9492b2fe8e5004ed800d13c8
Moacir Guedes <guedesmoacir12@gmail.com>
2023-09-19 17:49:01 -0300

    feat: router setup

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      name: 'About',
+      component: () => import('@/views/AboutView.vue')
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const pageTitle = typeof to.name === 'string' ? to.name : 'Default Title'
+  document.title = pageTitle
+  next()
 })
 
 export default router
